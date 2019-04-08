476e68265d5bd1a346563ee92dfbcc25dafd6516
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-08 02:24:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = welcomeMessage.slice(0,7);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
